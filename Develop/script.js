be998b4bb8a2f0ffaac1452325936e58e60aa773
120kjobplease <72399013+120kjobplease@users.noreply.github.com>
2021-04-23 16:22:05 -0400

    red date today

--- Develop/script.js
+++ Develop/script.js
@@ -1,90 +1,128 @@
-//DEPENDENCIES
-
+// TIMEEEEEE
 var today = moment();
-$("#la").text(today.format("MM Do, YYYY"))
-var rightnow = moment("6/28/21").format('MMMM Do YYYY');
-$("#time").text(rightnow)
-// STARTING DATA
-//list of appointments
-var appointments = [
-    {
-        time: 8,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour" //make your times go to different colors...
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
-    }
-    {
-        time: 9,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
+// going to iterate thru every time
+var tasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
 
-    }
-    {
-        time: 10,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 11,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 12,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 13,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 14,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 15,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 16,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-    {
-        time: 17,
-        topic: "Dynamically Rendering with jQuery",
-        duration: "1 hour"
-
-    }
-]
-//FUNCTIONS
-//render appointments... also gonna need a event.preventdefault
-function renderAppointments(){
-    console.log("rendering appointments")
-    //go thru list of appointments
-
+var setTasks = function() {
+   
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-var appointmentCard = $('<div class="cardappointment">');
-var cardbody = $('<div class="cardbody">');
 
-// USER INTERACTIONS
+var getTasks = function() {
+    
 
-//INTIALIZATIONS
-//render the appointment
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
 
-renderAppointments();
+        // for each key/value pair in tasks, create a task
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
 
-//if moment() > id.value
-//then set class to past
+    // make sure the past/current/future time is reflected
+    auditTasks()
+}
 
+var createTask = function(taskText, hourDiv) {
+    /* create a task in the row that corresponds to the specified hour */
+
+    var taskDiv = hourDiv.find(".task");
+    var taskP = $("<p>")
+        .addClass("description")
+        .text(taskText)
+    taskDiv.html(taskP);
+}
+
+var auditTasks = function() {
+    /* update the background of each row based on the time of day */
+
+    var currentHour = moment().hour();
+    $(".task-info").each( function() {
+        var elementHour = parseInt($(this).attr("id"));
+
+        // handle past, present, and future
+        if ( elementHour < currentHour ) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        }
+        else if ( elementHour === currentHour ) {
+            $(this).removeClass(["past", "future"]).addClass("present");
+        }
+        else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+    })
+};
+
+var replaceTextarea = function(textareaElement) {
+   
+
+    
+    var taskInfo = textareaElement.closest(".task-info");
+    var textArea = taskInfo.find("textarea");
+
+    
+    var time = taskInfo.attr("id");
+    var text = textArea.val().trim();
+
+
+    tasks[time] = [text]; 
+    setTasks();
+
+    
+    createTask(text, taskInfo);
+}
+
+/* CLICK HANDLERS */
+
+// tasks
+$(".task").click(function() {
+
+    // save the other tasks if they've already been clicked
+    $("textarea").each(function() {
+        replaceTextarea($(this));
+    })
+
+    // convert to a textarea element if the time hasn't passed
+    var time = $(this).closest(".task-info").attr("id");
+    if (parseInt(time) >= moment().hour()) {
+
+        // create a textInput element that includes the current task
+        var text = $(this).text();
+        var textInput = $("<textarea>")
+            .addClass("form-control")
+            .val(text);
+
+        // add the textInput element to the parent div
+        $(this).html(textInput);
+        textInput.trigger("focus");
+    }
+})
+
+// save button click handler
+$(".saveBtn").click(function() {
+    replaceTextarea($(this));
+})
+
+// update task backgrounds on the hour
+timeToHour = 3600000 - today.milliseconds();  // check how much time is left until the next hour
+setTimeout(function() {
+    setInterval(auditTasks, 3600000)
+}, timeToHour);
+
+// get the tasks from localStorage on load.
+getTasks();
